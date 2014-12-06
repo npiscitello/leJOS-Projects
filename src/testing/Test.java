e3af65d234cf03df30367c39b0f8bd9f3508e621
Nick P <nb.piscitello@gmail.com>
2014-12-06 18:29:19 -0500

    first commit

---
+++ src/testing/Test.java
@@ -0,0 +1,13 @@
+package testing;
+
+import lejos.nxt.Button;
+
+public class Test {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Button.waitForAnyPress();
+    }
+}
