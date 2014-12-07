6c8986dba482f6e0aff3b1ddbbccf9a4d0db506d
nicholaspiscitello <nicholaspiscitello@192.168.1.10>
2014-12-06 19:51:07 -0500

    Working version - good to NXT

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -1,13 +1,9 @@
 package testing;
-
 import lejos.nxt.Button;
 
 public class Test {
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Button.waitForAnyPress();
-    }
+  public static void main (String[] args) {
+    System.out.println("Hello World");
+    Button.waitForAnyPress();
+  }
 }
