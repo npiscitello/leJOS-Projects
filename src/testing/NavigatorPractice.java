f64d28a7f3858eee60524a0bbc7d5f59219cf816
Nick P <nb.piscitello@gmail.com>
2014-12-09 15:08:19 -0500

    Beginning navigation training; saving to get back to later

---
+++ src/testing/NavigatorPractice.java
@@ -0,0 +1,13 @@
+package testing;
+
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.Navigator;
+import lejos.robotics.navigation.CompassPilot;
+
+
+public class NavigatorPractice {
+	public static void main(String[] args)	{
+		
+	}
+}
