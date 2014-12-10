9f768a2442278b7e3fd3313de82deee03182ec01
Nick P <nb.piscitello@gmail.com>
2014-12-09 21:38:57 -0500

    Added comments to NavPractice

--- src/testing/NavPractice.java
+++ src/testing/NavPractice.java
@@ -4,24 +4,24 @@
 // gear ratio: 6/10 = .6 (wheel does .6 rotations for every motor rotation)
 // apparent wheel circumference: .6(10.21018) = 6.12611
 
-package testing;
+package testing;													// what the class belongs to
 
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
-import lejos.robotics.navigation.Navigator;
-import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.Navigator;							// importing other classes; like
+import lejos.robotics.navigation.DifferentialPilot;					// libraries in C
 import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.navigation.Pose;
 
 public class NavPractice {
-	static float diameter = 1.95f, width = 4.875f;
-	static int speed = 100;
+	static float diameter = 1.95f, width = 4.875f;					// variable declaration and
+	static int speed = 100;											// object creation
 	static DifferentialPilot pilot = new DifferentialPilot(diameter, width, Motor.B, Motor.C, true);
 	static OdometryPoseProvider pose = new OdometryPoseProvider(pilot);
 	static Pose current = new Pose();
 	static Navigator nav = new Navigator(pilot);
 	
-	public static void setNavigatorPath(int distance)	{
+	public static void setNavigatorPath(int distance)	{			// path assignment for navigator
 		nav.addWaypoint(distance, 0);
 		nav.addWaypoint(0, -distance);
 		nav.addWaypoint(-distance, 0);
@@ -29,10 +29,10 @@ public class NavPractice {
 		nav.addWaypoint(0, 0, 0);
 	}
 	
-	public static void goRotate(int angle)	{
-		pose.setPose(current);
-		pilot.rotate(angle);
-		NavPractice.printCurrentPose();
+	public static void goRotate(int angle)	{						// rotate instructions
+		pose.setPose(current);										// initialize pose
+		pilot.rotate(angle);										// move
+		NavPractice.printCurrentPose();								// call output method
 		pilot.steer(100, -180);
 		NavPractice.printCurrentPose();
 		pilot.rotate(angle);
@@ -40,21 +40,21 @@ public class NavPractice {
 		return;
 	}
 	
-	public static void printCurrentPose()	{
-		current = pose.getPose();
-		//System.out.println(current.getX());
+	public static void printCurrentPose()	{						// output method;
+		current = pose.getPose();									// get current pose
+		//System.out.println(current.getX());						// print current data
 		//System.out.println(current.getY());
 		System.out.println(current.getHeading());
 		System.out.println("");
 	}
 
-	public static void main(String[] args)	{
-		pilot.addMoveListener(pose);
+	public static void main(String[] args)	{						// main method; executed first
+		pilot.addMoveListener(pose);								// enable pose tracking
 		pilot.setTravelSpeed(speed);
 		Button.waitForAnyPress();
-		//NavPractice.goRotate(90);
-		NavPractice.setNavigatorPath(10);
-		nav.followPath();
+		//NavPractice.goRotate(90);									// call rotate method
+		NavPractice.setNavigatorPath(10);							// call path assignment method
+		nav.followPath();											// execute path
 		Button.waitForAnyPress();
 	}
 }
